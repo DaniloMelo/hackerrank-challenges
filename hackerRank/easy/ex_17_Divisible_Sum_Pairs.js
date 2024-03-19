1a2f762c27028801026556d77c9985708ff4e06e
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-03-18 21:15:03 -0300

    fix: divisible sum pairs

--- hackerRank/easy/ex_17_Divisible_Sum_Pairs.js
+++ hackerRank/easy/ex_17_Divisible_Sum_Pairs.js
@@ -1,7 +1,7 @@
 /*
   Link: https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
-
+  
 */
 
 function divisibleSumPairs(n, k, ar) {
@@ -9,11 +9,8 @@ function divisibleSumPairs(n, k, ar) {
 
   for (let i = 0; i < n; i++) {
     for (let j = i + 1; j < n; j++) {
-      console.log(`i:${ar[i]}, j:${ar[j]}`)
-
       if ((ar[i] + ar[j]) % k === 0) {
         count += 1
-        console.log(count)
       }
     }
   }
@@ -22,7 +19,7 @@ function divisibleSumPairs(n, k, ar) {
 }
 
 console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])); // resp: 5
-// console.log(divisibleSumPairs(6, 5, [1, 2, 3, 4, 5, 6])) // resp: 3
+console.log(divisibleSumPairs(6, 5, [1, 2, 3, 4, 5, 6])) // resp: 3
 
 
 
